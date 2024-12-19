b098e3e65e038cbdc2153bc238b49d5bbe5d367a
siloneco <siloneco4646@gmail.com>
2024-12-19 13:15:23 +0900

    feat: convert theme change button to toggle button

--- src/components/layout/MainSidebar/index.tsx
+++ src/components/layout/MainSidebar/index.tsx
@@ -5,12 +5,6 @@ import {
   SidebarGroupContent,
 } from '@/components/ui/sidebar'
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
 import TypeSelector from '../TypeSelector'
 import AvatarRelatedAssetFilter from './layout/AvatarRelatedAssetFilter'
 import { Button } from '@/components/ui/button'
@@ -33,6 +27,10 @@ const MainSidebar = () => {
     document.location.href = `/about`
   }
 
+  const onClick = () => {
+    setTheme?.((theme) => (theme === 'dark' ? 'light' : 'dark'))
+  }
+
   return (
     <Sidebar>
       <SidebarContent>
@@ -40,30 +38,16 @@ const MainSidebar = () => {
           <div className="m-2 border-2 text-lg p-1 px-4 rounded-lg">
             KonoAsset
           </div>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button
-                variant="outline"
-                size="icon"
-                className="ml-auto mr-2 flex relative"
-              >
-                <Sun className="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                <span className="sr-only">Toggle theme</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuItem onClick={() => setTheme('light')}>
-                Light
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => setTheme('dark')}>
-                Dark
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => setTheme('system')}>
-                System
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <Button
+            variant="outline"
+            size="icon"
+            className="ml-auto mr-2 flex relative"
+            onClick={onClick}
+          >
+            <Sun className="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
         </div>
         <SidebarGroup>
           <SidebarGroupContent className="p-2">
