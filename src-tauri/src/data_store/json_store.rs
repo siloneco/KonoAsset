e5bb61316e62fe11ae4da6c5dc74b8848b8324f7
siloneco <siloneco4646@gmail.com>
2024-12-22 22:14:26 +0900

    feat: auto save when booth item info migrated

--- src-tauri/src/data_store/json_store.rs
+++ src-tauri/src/data_store/json_store.rs
@@ -58,112 +58,145 @@ impl<T: AssetTrait + Clone + Serialize + DeserializeOwned + Eq + Hash> JsonStore
             return Err("Failed to open file".into());
         }
 
-        let mut assets = self.assets.lock().unwrap();
-        let result: Result<HashSet<T>, serde_json::Error> =
-            serde_json::from_reader(file_open_result.unwrap());
+        let mut require_save_flag = false;
 
-        if result.is_err() {
-            return Err("Failed to deserialize file".into());
-        }
+        {
+            let mut assets = self.assets.lock().unwrap();
+            let result: Result<HashSet<T>, serde_json::Error> =
+                serde_json::from_reader(file_open_result.unwrap());
 
-        let deserialized = result.unwrap();
-        let mut result = HashSet::new();
-        deserialized.into_iter().for_each(|mut item| {
-            let url = item.get_description().booth_url.as_ref();
-
-            if url.is_some() {
-                let item_id = extract_booth_item_id_from_booth_url(url.unwrap());
-
-                if item_id.is_ok() {
-                    item.get_description_as_mut().booth_item_id = Some(item_id.unwrap());
-                }
+            if result.is_err() {
+                return Err("Failed to deserialize file".into());
             }
 
-            result.insert(item);
-        });
+            let deserialized = result.unwrap();
+            let mut result = HashSet::new();
+            deserialized.into_iter().for_each(|mut item| {
+                let url = item.get_description().booth_url.as_ref();
 
-        *assets = result;
+                if url.is_some() {
+                    let item_id = extract_booth_item_id_from_booth_url(url.unwrap());
+
+                    if item_id.is_ok() {
+                        item.get_description_as_mut().booth_item_id = Some(item_id.unwrap());
+                        require_save_flag = true;
+                    }
+                }
+
+                result.insert(item);
+            });
+
+            *assets = result;
+        }
+
+        if require_save_flag {
+            let save_result = self.save();
+
+            if save_result.is_err() {
+                return Err(save_result.err().unwrap());
+            }
+        }
 
         Ok(())
     }
 
     pub fn add_asset_and_save(&self, asset: T) -> Result<(), String> {
-        let mut assets = self.assets.lock().unwrap();
-        assets.insert(asset.clone());
-
-        let mut path = self.app_data_dir.clone();
-        path.push("metadata");
-        path.push(T::filename());
-
-        let file_open_result = File::create(path);
-
-        if file_open_result.is_err() {
-            return Err("Failed to open file".into());
+        {
+            let mut assets = self.assets.lock().unwrap();
+            assets.insert(asset.clone());
         }
 
-        let result = serde_json::to_writer(file_open_result.unwrap(), &*assets);
-
-        match result {
-            Ok(_) => Ok(()),
-            Err(e) => Err(format!("Failed to serialize file: {}", e)),
-        }
+        self.save()
     }
 
     pub fn update_asset_and_save(&self, mut asset: T) -> Result<(), String> {
-        let mut assets = self.assets.lock().unwrap();
-        let old_asset = assets
-            .iter()
-            .find(|a| a.get_id() == asset.get_id())
-            .cloned();
+        {
+            let mut assets = self.assets.lock().unwrap();
+            let old_asset = assets
+                .iter()
+                .find(|a| a.get_id() == asset.get_id())
+                .cloned();
 
-        if old_asset.is_none() {
-            return Err("Asset not found".into());
-        }
+            if old_asset.is_none() {
+                return Err("Asset not found".into());
+            }
 
-        // update の時は created_at を更新しない
-        asset.get_description_as_mut().created_at =
-            old_asset.as_ref().unwrap().get_description().created_at;
+            // update の時は created_at を更新しない
+            asset.get_description_as_mut().created_at =
+                old_asset.as_ref().unwrap().get_description().created_at;
 
-        let old_asset = old_asset.unwrap();
+            let old_asset = old_asset.unwrap();
 
-        if old_asset.get_description().image_src != asset.get_description().image_src {
-            let old_image = &old_asset.get_description().image_src;
-            let new_image = &asset.get_description().image_src;
+            if old_asset.get_description().image_src != asset.get_description().image_src {
+                let old_image = &old_asset.get_description().image_src;
+                let new_image = &asset.get_description().image_src;
 
-            if old_image.is_some() {
-                let delete_result =
-                    delete_asset_image(&self.app_data_dir, old_image.as_ref().unwrap());
+                if old_image.is_some() {
+                    let delete_result =
+                        delete_asset_image(&self.app_data_dir, old_image.as_ref().unwrap());
 
-                if delete_result.is_err() {
-                    return Err(delete_result.err().unwrap());
+                    if delete_result.is_err() {
+                        return Err(delete_result.err().unwrap());
+                    }
+
+                    if !delete_result.unwrap() {
+                        return Err("Failed to delete old image".into());
+                    }
                 }
 
-                if !delete_result.unwrap() {
-                    return Err("Failed to delete old image".into());
+                if new_image.is_some() {
+                    let mut path = self.app_data_dir.clone();
+                    path.push("images");
+                    path.push(format!("{}.jpg", Uuid::new_v4().to_string()));
+
+                    let result = execute_image_fixation(new_image.as_ref().unwrap(), &path);
+
+                    if result.is_err() {
+                        return Err(result.err().unwrap());
+                    }
+
+                    if result.unwrap() {
+                        asset.get_description_as_mut().image_src =
+                            Some(path.to_str().unwrap().to_string());
+                    }
                 }
             }
 
-            if new_image.is_some() {
-                let mut path = self.app_data_dir.clone();
-                path.push("images");
-                path.push(format!("{}.jpg", Uuid::new_v4().to_string()));
-
-                let result = execute_image_fixation(new_image.as_ref().unwrap(), &path);
-
-                if result.is_err() {
-                    return Err(result.err().unwrap());
-                }
-
-                if result.unwrap() {
-                    asset.get_description_as_mut().image_src =
-                        Some(path.to_str().unwrap().to_string());
-                }
-            }
+            assets.remove(&old_asset);
+            assets.insert(asset.clone());
         }
 
-        assets.remove(&old_asset);
-        assets.insert(asset.clone());
+        let save_result = self.save();
 
+        if save_result.is_err() {
+            return Err(save_result.err().unwrap());
+        }
+
+        Ok(())
+    }
+
+    pub fn delete_asset_and_save(&self, id: Uuid) -> Result<bool, String> {
+        {
+            let mut assets = self.assets.lock().unwrap();
+            let asset = assets.iter().find(|asset| asset.get_id() == id).cloned();
+
+            if asset.is_none() {
+                return Ok(false);
+            }
+
+            assets.remove(&asset.unwrap());
+        }
+
+        let save_result = self.save();
+
+        if save_result.is_err() {
+            return Err(save_result.err().unwrap());
+        }
+
+        Ok(true)
+    }
+
+    fn save(&self) -> Result<(), String> {
         let mut path = self.app_data_dir.clone();
         path.push("metadata");
         path.push(T::filename());
@@ -174,37 +207,11 @@ impl<T: AssetTrait + Clone + Serialize + DeserializeOwned + Eq + Hash> JsonStore
             return Err("Failed to open file".into());
         }
 
+        let assets = self.assets.lock().unwrap();
         let result = serde_json::to_writer(file_open_result.unwrap(), &*assets);
 
         match result {
             Ok(_) => Ok(()),
-            Err(e) => Err(format!("Failed to serialize file: {}", e)),
-        }
-    }
-
-    pub fn delete_asset_and_save(&self, id: Uuid) -> Result<bool, String> {
-        let mut assets = self.assets.lock().unwrap();
-        let asset = assets.iter().find(|asset| asset.get_id() == id).cloned();
-
-        if asset.is_none() {
-            return Ok(false);
-        }
-
-        assets.remove(&asset.unwrap());
-
-        let mut path = self.app_data_dir.clone();
-        path.push("metadata");
-        path.push(T::filename());
-
-        let file_open_result = File::create(path);
-        if file_open_result.is_err() {
-            return Err("Failed to open file".into());
-        }
-
-        let result = serde_json::to_writer(file_open_result.unwrap(), &*assets);
-
-        match result {
-            Ok(_) => Ok(true),
             Err(e) => Err(format!("Failed to serialize file: {}", e)),
         }
     }
