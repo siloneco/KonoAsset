a35f183961c1a2c07f7f603c0b73152554b48415
siloneco <siloneco4646@gmail.com>
2024-12-23 22:50:41 +0900

    feat: click badge to apply asset type filter

--- src/components/part/AssetBadge/index.tsx
+++ src/components/part/AssetBadge/index.tsx
@@ -4,33 +4,34 @@ import { AssetType } from '@/lib/entity'
 type Props = {
   className?: string
   type: AssetType
+  onClick?: () => void
 }
 
-const AssetBadge = ({ type, className }: Props) => {
+const AssetBadge = ({ type, className, onClick }: Props) => {
   if (type === AssetType.Avatar) {
     return (
-      <Badge variant="avatar" className={className}>
+      <Badge variant="avatar" className={className} onClick={onClick}>
         アバター素体
       </Badge>
     )
   }
   if (type === AssetType.AvatarRelated) {
     return (
-      <Badge variant="avatarRelated" className={className}>
+      <Badge variant="avatarRelated" className={className} onClick={onClick}>
         アバター関連
       </Badge>
     )
   }
   if (type === AssetType.World) {
     return (
-      <Badge variant="world" className={className}>
+      <Badge variant="world" className={className} onClick={onClick}>
         ワールドアセット
       </Badge>
     )
   }
 
   return (
-    <Badge variant="outline" className={className}>
+    <Badge variant="outline" className={className} onClick={onClick}>
       不明
     </Badge>
   )
