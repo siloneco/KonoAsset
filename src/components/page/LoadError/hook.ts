250c3425a752eff1472fdbc525e24ee9a915a272
siloneco <siloneco4646@gmail.com>
2025-03-23 20:30:08 +0900

    feat: add language selection dropdown to LoadError page

--- src/components/page/LoadError/hook.ts
+++ src/components/page/LoadError/hook.ts
@@ -1,12 +1,17 @@
-import { commands } from '@/lib/bindings'
+import { PreferenceContext } from '@/components/context/PreferenceContext'
+import { commands, LanguageCode } from '@/lib/bindings'
+import { useContext } from 'react'
 
 type ReturnProps = {
   openAppDir: () => void
   openAssetDir: () => void
   openMetadataDir: () => void
+  onLanguageChanged: (value: LanguageCode) => Promise<void>
 }
 
 export const useLoadErrorPage = (): ReturnProps => {
+  const { preference, setPreference } = useContext(PreferenceContext)
+
   const openAppDir = async () => {
     await commands.openAppDir()
   }
@@ -19,5 +24,14 @@ export const useLoadErrorPage = (): ReturnProps => {
     await commands.openMetadataDir()
   }
 
-  return { openAppDir, openAssetDir, openMetadataDir }
+  const onLanguageChanged = async (value: LanguageCode) => {
+    setPreference({ ...preference, language: value }, false)
+  }
+
+  return {
+    openAppDir,
+    openAssetDir,
+    openMetadataDir,
+    onLanguageChanged,
+  }
 }
