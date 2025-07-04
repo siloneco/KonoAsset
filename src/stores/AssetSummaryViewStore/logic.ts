9889a6a6addf33f91115ad1e639170654ee55531
siloneco <siloneco4646@gmail.com>
2025-07-05 02:51:24 +0900

    perf: remove unused function

--- src/stores/AssetSummaryViewStore/logic.ts
+++ src/stores/AssetSummaryViewStore/logic.ts
@@ -1,5 +1,4 @@
 import { SortBy, AssetSummary, commands, AppState } from '@/lib/bindings'
-import { DEFAULT_APP_STATE } from './index.constants'
 
 export const refreshAssetSummaries = async (
   sortBy: SortBy,
@@ -9,17 +8,6 @@ export const refreshAssetSummaries = async (
   if (result.status === 'error') {
     console.error(result.error)
     return []
-  }
-
-  return result.data
-}
-
-export const fetchAppState = async (): Promise<AppState> => {
-  const result = await commands.getAppState()
-
-  if (result.status === 'error') {
-    console.error(result.error)
-    return DEFAULT_APP_STATE
   }
 
   return result.data
